5a0ea48a7cd1f926febbbd93825a07506e669cb2
tgoandrex <tgoandrex@gmail.com>
2023-07-02 16:59:11 -0500

    Initial commit

---
+++ app/not-found.tsx
@@ -0,0 +1,3 @@
+export default function NotFound() {
+    return <h1 className="text-3xl font-bold underline">Could not find requested resource</h1>
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">Hello, Index!</h1>
+            <a href="/test">Test Page</a>
+        </>
+    )
+}
