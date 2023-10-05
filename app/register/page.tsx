51f51cbae26f0b15dd4d2163b351e6b4bc512b3a
tgoandrex <tgoandrex@gmail.com>
2023-10-05 18:45:38 -0500

    Test

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,7 @@
 const Register = () => {
   return (
     <main className="padding border border-black">
-      <h1 className="text-3xl font-bold underline">Hello, Registration Page!</h1>
+      <h1 className="text-3xl font-bold underline">Hello, Register Page!</h1>
     </main>
   )
 }
